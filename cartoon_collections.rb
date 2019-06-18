6389c38dfd70b0281873899fb349d22a157072d2
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-18 16:56:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,12 +6,15 @@ def roll_call_dwarves(array) # code an argument here
 end
 
 def summon_captain_planet(array)
-  array.each do |planet| 
+  array.map do |planet| 
     planet.capitalize + '!'
   end
   # code an argument here
   # Your code here
 end
+
+
+
 
 def long_planeteer_calls(array)
   array.any? do |word| 
