c39b2caf09fe54e83e8a97d7c522b5889e7ca9c9
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-18 16:32:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array) # code an argument here
  array.each_with_index do |name, index|
-   puts "#{name} #{index + 1}"
+   puts "#{index + 1}. #{name}"
  end
    # Your code here
 end
