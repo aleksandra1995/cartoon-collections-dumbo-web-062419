1829f0012429c907f0d511d5df59bebf4663cc1f
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-18 16:51:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_the_cheese(array)
   array.find do |cheese|
-    cheese.select?(cheese_types)
+    cheese_types.select?(cheese)
   end
   # code an argument here
   # the array below is here to help
