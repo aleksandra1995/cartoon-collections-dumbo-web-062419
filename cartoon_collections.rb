f8292497faf0ccfdaaf8eda30ea910f4574b8f98
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-18 16:39:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.select |word| 
-  word == 4
+  array.any? |word| 
+  word.length == 4
 end
   # code an argument here
   # Your code here
